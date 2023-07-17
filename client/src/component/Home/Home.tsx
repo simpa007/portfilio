0cef767cacdc25220feba92ff7e44ff424c46cc2
Simpa Emmanuel <simpaimmey@gmail.com>
2023-07-17 16:52:30 +0100

    updates

--- client/src/component/Home/Home.tsx
+++ client/src/component/Home/Home.tsx
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./Home.scss";
-const logo = require("../../img/undraw-bgremove.png");
+const logo = require("../../img/bgremove.png");
 const react = require("../../img/physics.png");
 const nodejs = require("../../img/nodejs.png");
 const python = require("../../img/python.png");
+const wordpress = require("../../img/wordpress.png");
 export default function Home() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomText = getRadomText();
+      setText(randomText);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const getRadomText = () => {
+    const texts = [
+      "Web Developer",
+      "Front-end Developer",
+      "Full-Stack Developer",
+    ];
+    let randomIndex = Math.floor(Math.random() * texts.length);
+    return texts[randomIndex];
+  };
   return (
     <div id="home">
       <div className="container container-home">
         <div className="row row-home">
-          <div
-            className="col-md-6 col-sm-12 col-home"
-            // style={{ border: "2px solid red" }}
-          >
+          <div className="col-md-6 col-sm-12 col-home">
             <div className="home-body">
               <button
                 type="button"
                 style={{
                   outline: "none",
                   color: "#fff",
-                  // borderColor: "#6ff4a5",
                 }}
                 className="btn btn-outline-success btn-lg"
               >
-                Front-End Developer
+                {text}
               </button>
+
               <h1 className="home-text">
                 Talk is Cheap.
                 <br />
@@ -40,7 +57,6 @@ export default function Home() {
                 <a href="https://wa.me/08073696557">LET'S CHAT!</a>
               </p>
             </div>
-
             <div className="experience">
               <h1>
                 3<sup>+</sup>
@@ -64,6 +80,12 @@ export default function Home() {
                 <img src={python} alt="python" className="python" width={50} />
                 <img src={react} alt="react" className="react" width={50} />
                 <img src={nodejs} alt="nodejs" className="nodejs" width={50} />
+                <img
+                  src={wordpress}
+                  alt="wordpress"
+                  className="wordpress"
+                  width={50}
+                />
               </div>
             </div>
           </div>
